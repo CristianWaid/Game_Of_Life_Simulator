d79443c228da1f53aeee4eda063d936e026f5404
cristian.waid <cristian.waid@reservix.de>
2019-06-27 13:13:05 +0200

    add Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+from src.game_of_life_visual.main_window import MainWindow
+
+root = Tk()
+root.attributes('-fullscreen', True)
+root.config(bg="gray")
+root.title("Game of Life")
+
+game_of_life = MainWindow(root, 600, 10)
+
+root.mainloop()
