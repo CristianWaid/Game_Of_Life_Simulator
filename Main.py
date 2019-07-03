3448a8d3a8752ab14cad9e3f80e4e99a4d9d0839
cristian.waid <cristian.waid@reservix.de>
2019-07-03 19:19:00 +0200

    add window logo

--- Main.py
+++ Main.py
@@ -3,8 +3,11 @@ from tkinter import *
 from src.game_of_life_visual.main_window import MainWindow
 
 root = Tk()
-# root.attributes('-fullscreen', True)
 root.geometry("800x750")
+
+icon = PhotoImage(file="src/game_of_life_visual/icons/icon.png")
+root.tk.call("wm", "iconphoto", root._w, icon)
+
 root.config(bg="white")
 root.resizable(False, False)
 root.title("Game of Life")
